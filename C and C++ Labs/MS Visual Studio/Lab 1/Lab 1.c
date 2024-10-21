ead4abc506b13081c526098b6e1e8fcec74aee45
Cyube <77444347+LyCh1e@users.noreply.github.com>
2024-10-21 22:04:41 +0800

    Add files via upload

---
+++ C and C++ Labs/MS Visual Studio/Lab 1/Lab 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int p1Number = 0;
+    int roundsLeft = 10;
+    int p2Number = 0;
+
+    while(true) {
+        printf("Player 1, enter a number between 1 and 1000:\n");
+        scanf_s("%d", &p1Number);
+        if (p1Number <= 1 || p1Number >= 1000) {
+            printf("That number is out of range.\n");
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+
+    while(roundsLeft > 0)
+    {
+        printf("Player 2, you have %d guesses remaining.\n", roundsLeft);
+        printf("Enter your guess:\n");
+        scanf_s("%d", &p2Number);
+        if (p2Number > p1Number)
+        {
+            printf("Too high.\n");
+        }
+        else if (p2Number < p1Number)
+        {
+            printf("Too low.\n");
+        }
+        else if (p2Number == p1Number)
+        {
+            printf("Player 2 wins.\n");
+            return 0;
+        }
+        roundsLeft--;
+    }
+    printf("Player 1 wins.\n");
+    return 0;
+}
