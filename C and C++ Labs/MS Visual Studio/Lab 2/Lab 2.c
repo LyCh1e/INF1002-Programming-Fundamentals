6f8fa77c96d2abeb859fb802007f2733644b6645
Cyube <77444347+LyCh1e@users.noreply.github.com>
2024-10-29 18:00:33 +0800

    Update Lab 2.c

--- C and C++ Labs/MS Visual Studio/Lab 2/Lab 2.c
+++ C and C++ Labs/MS Visual Studio/Lab 2/Lab 2.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
-int findPattern(const char* inputPattern, const char* inputText, int caseSensitiveValue)
+int findPattern(const char *inputPattern, const char *inputText, char caseSensitiveValue)
 {
     int textLength = strlen(inputText);
     int patternLength = strlen(inputPattern);
@@ -19,7 +19,7 @@ int findPattern(const char* inputPattern, const char* inputText, int caseSensiti
             char textletter = inputText[textIndex + patternIndex];
             char patternLetter = inputPattern[patternIndex];
 
-            if (!caseSensitiveValue) // Check if case sensitivity is to be applied or not
+            if (caseSensitiveValue == 'N') // Check if case sensitivity is to be applied or not
             {
                 textletter = tolower(textletter);
                 patternLetter = tolower(patternLetter);
@@ -29,14 +29,14 @@ int findPattern(const char* inputPattern, const char* inputText, int caseSensiti
             {
                 continue;
             }
-            else if (patternLetter == '_') // Else if the letter is a blank space
+            else if (patternLetter == '_' || patternLetter == ' ') // Else if the letter is a blank space
             {
                 if (!isspace(textletter))
                 {
                     matchCase = false;
                     break;
                 }
-            }
+            } 
             else
             {
                 if (textletter != patternLetter)
@@ -52,7 +52,7 @@ int findPattern(const char* inputPattern, const char* inputText, int caseSensiti
             return textIndex; // Return the position where the match was found in the array
         }
     }
-    return -1; // No match found in the text array
+    return 1; // No match found in the text array
 }
 
 int main()
@@ -60,7 +60,7 @@ int main()
     char inputText[256];
     char inputPattern[256];
     char caseSensitiveInput;
-    int caseSensitiveValue;
+    char caseSensitiveValue;
     int matchedPattern = 0;
 
     // Ask for strings to match
@@ -73,18 +73,18 @@ int main()
 
     // Ask for case sesitivity
     printf("Should the match be case-sensitive? (Y/N):\n");
-    scanf_s("%c", &caseSensitiveInput);
+    scanf("%c", &caseSensitiveInput);
 
     // Remove newline characters ("\n") if they exist
     inputText[strcspn(inputText, "\n")] = 0;
     inputPattern[strcspn(inputPattern, "\n")] = 0;
 
-    // caseSensitiveValue = 1 (true) if 'Y' and caseSensitiveValue = 0 (false) if 'N'
+    // Set caseSensitiveValue = 'Y' or  caseSensitiveValue = 'N' from their lower case
     caseSensitiveValue = toupper(caseSensitiveInput);
 
     matchedPattern = findPattern(inputPattern, inputText, caseSensitiveValue);
 
-    if (matchedPattern != -1)
+    if (matchedPattern != 1)
     {
         printf("Matches at position %d.\n", matchedPattern);
     }
